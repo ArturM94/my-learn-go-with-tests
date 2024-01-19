369546f4fb87010de5608ab9756af873143ee56a
Artur Manukian <manukian.artur.94@gmail.com>
2024-01-19 19:04:50 +0200

    exercise: caller can specify how many times the character is repeated in Repeat func

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,15 +3,34 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeat 'a' 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertRepeat(t, repeated, expected)
+	})
+	t.Run("repeat 'x' 12 times", func(t *testing.T) {
+		repeated := Repeat("x", 12)
+		expected := "xxxxxxxxxxxx"
+		assertRepeat(t, repeated, expected)
+	})
+}
+
+func assertRepeat(t testing.TB, repeated, expected string) {
+	t.Helper()
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
 
 func BenchmarkRepeat(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Repeat("a")
-	}
+	b.Run("repeat 'x' 5 times", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			Repeat("x", 5)
+		}
+	})
+	b.Run("repeat 'a' 10 times", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			Repeat("a", 10)
+		}
+	})
 }
