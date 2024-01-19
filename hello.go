9ba17a2ecd901a52b6a4b1882efc7d28741c5aad
Artur Manukian <manukian.artur.94@gmail.com>
2024-01-19 15:35:06 +0200

    refactor: add default name to Hello func

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const helloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		return helloPrefix + "World"
+	}
+	return helloPrefix + name
 }
 
 func main() {
--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Artur")
-	want := "Hello, Artur"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Artur")
+		want := "Hello, Artur"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// t.Helper() define assertCorrectMessage function as a helper
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
