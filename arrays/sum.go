f82a553744b3602991d31aa37661fd31324c7559
Artur Manukian <manukian.artur.94@gmail.com>
2024-01-20 19:15:15 +0200

    feat: add Sum all func

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,12 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lenOfNumbers := len(numbersToSum)
+	sums := make([]int, lenOfNumbers)
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
+}
