1b63d8cb79f5e74ceb4253246102b6dfc5e8ad2b
Artur Manukian <manukian.artur.94@gmail.com>
2024-01-20 18:46:17 +0200

    refactor: make Sum func to work with slices instead of arrays

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,10 +3,12 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	nubmers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(nubmers)
-	want := 15
-	if got != want {
-		t.Errorf("got %d want %d given %v", got, want, nubmers)
-	}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		nubmers := []int{1, 2, 3, 4, 5}
+		got := Sum(nubmers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, nubmers)
+		}
+	})
 }
