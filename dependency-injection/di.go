6304ad7d43b91ea2503903351f05927a4a6740d4
Artur Manukian <manukian.artur.94@gmail.com>
2024-01-25 20:58:11 +0200

    refactor: replace Buffer with more general Writer interface for Greet func

--- dependency-injection/di.go
+++ dependency-injection/di.go
@@ -1,10 +1,23 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func GreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "World")
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
+
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(GreeterHandler)))
 }
