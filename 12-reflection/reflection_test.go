55c7c79e57c91be038a8fe977811d6bcbcc9996e
Artur Manukian <manukian.artur.94@gmail.com>
2024-02-01 17:38:22 +0200

    refactor: make walk func handle maps

--- 12-reflection/reflection_test.go
+++ 12-reflection/reflection_test.go
@@ -76,6 +76,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Dnipro", "London"},
 		},
+		{
+			"maps",
+			map[string]string{
+				"Cow":   "Moo",
+				"Sheep": "Baa",
+			},
+			[]string{"Moo", "Baa"},
+		},
 	}
 
 	for _, test := range cases {
