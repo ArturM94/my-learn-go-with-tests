2437d0f23d5933964b951055f65cf61cd8190a0c
Artur Manukian <manukian.artur.94@gmail.com>
2024-02-05 18:22:57 +0200

    fix: typo

--- 10-concurrency/check_websites_test.go
+++ 10-concurrency/check_websites_test.go
@@ -38,7 +38,7 @@ func slowStubWebsiteChecker(_ string) bool {
 	return true
 }
 
-func BenchmarkCheclWebsite(b *testing.B) {
+func BenchmarkCheckWebsite(b *testing.B) {
 	urls := make([]string, 100)
 	for i := 0; i < len(urls); i++ {
 		urls[i] = "a url"
