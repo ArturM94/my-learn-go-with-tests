4060fe6a6dcd70c74d0cdbefcc9887328299c7c7
Artur Manukian <manukian.artur.94@gmail.com>
2024-02-15 18:30:48 +0200

    feat: add blogposts package to read posts from a file system

---
+++ 17-reading-files/blogposts_test.go
@@ -0,0 +1,20 @@
+package blogposts_test
+
+import (
+	blogposts "learn-go-with-tests/17-reading-files"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPost(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("hi")},
+		"hello-world2.md": {Data: []byte("hola")},
+	}
+
+	posts := blogposts.NewPostsFromFS(fs)
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
