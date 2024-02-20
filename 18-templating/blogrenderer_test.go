c54af161c4055c2a3c48f31dfef33e6ed548196c
Artur Manukian <manukian.artur.94@gmail.com>
2024-02-20 16:17:53 +0200

    refactor: improve performance by parsing templates only once

--- 18-templating/blogrenderer_test.go
+++ 18-templating/blogrenderer_test.go
@@ -3,6 +3,7 @@ package blogrenderer_test
 import (
 	"bytes"
 	approvals "github.com/approvals/go-approval-tests"
+	"io"
 	blogrenderer "learn-go-with-tests/18-templating"
 	"testing"
 )
@@ -17,13 +18,40 @@ func TestRender(t *testing.T) {
 		}
 	)
 
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
 
-		if err := blogrenderer.Render(&buf, aPost); err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
