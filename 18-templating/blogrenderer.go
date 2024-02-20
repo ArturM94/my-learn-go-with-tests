164f676381f312ec96e3221d004bd5071c6e8f2e
Artur Manukian <manukian.artur.94@gmail.com>
2024-02-20 17:12:57 +0200

    feat: add rendering the index of posts

--- 18-templating/blogrenderer.go
+++ 18-templating/blogrenderer.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"html/template"
 	"io"
+	"strings"
 )
 
 var (
@@ -14,6 +15,10 @@ var (
 type Post struct {
 	Title, Body, Description string
 	Tags                     []string
+}
+
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
 }
 
 type PostRenderer struct {
@@ -31,6 +36,21 @@ func NewPostRenderer() (*PostRenderer, error) {
 
 func (r *PostRenderer) Render(w io.Writer, p Post) error {
 	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{.SanitisedTitle}}">{{.Title}}</a></li>{{end}}</ol>`
+
+	templ, err := template.New("index").Parse(indexTemplate)
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, posts); err != nil {
 		return err
 	}
 
